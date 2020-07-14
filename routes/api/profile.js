cbfc689c195653f4405ee7504c7b566406fa0d2d
washiPolly <pollylsu@gmail.com>
2020-07-13 20:22:02 -0700

    finished delete route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,8 @@ const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator/check')
 
 const Profile = require('../../models/Profile')
-const User = require('../../models/User')
+const User = require('../../models/User');
+const { restart } = require('nodemon');
 
 // @route    GET api/profile/me
 // @desc     Get current user's profile
@@ -95,5 +96,60 @@ router.post('/', [auth, [
 
     }
 );
+
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+
+    }
+
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get all profile by user ID
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error')
+
+    }
+
+});
+
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove users posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await Profile.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+
+    }
+
+});
 
 module.exports = router;
